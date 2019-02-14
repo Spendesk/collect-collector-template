4b9f8336e31c207b06aac3ff074ba807b0a54ec4
Auree Aubert <contact@auree.me>
2019-02-14 20:03:07 +0100

    Update version of collect-node to 0.6

--- template/app/utils/get-invoices.js
+++ template/app/utils/get-invoices.js
@@ -1,31 +1,3 @@
-const Promise = require("bluebird");
-
-const getemailInvoicesUrlsEvaluator = lastCollectedAt => {
-  const lastCollectedDate = new Date(lastCollectedAt);
-  const invoiceUrls = [];
-
-  document
-    .querySelectorAll(".ibox-content>table>tbody>tr")
-    .forEach(billingTag => {
-      const rowTds = billingTag.querySelectorAll("td");
-      const data = {};
-      data.date = rowTds[1].textContent.replace("th", ",");
-      data.href = rowTds[3].querySelector("a").href;
-      const invoiceDate = new Date(data.date);
-      if (invoiceDate.getTime() >= lastCollectedDate.getTime()) {
-        invoiceUrls.push(data);
-      }
-    });
-
-  return invoiceUrls;
-};
-
-const getemailInvoicesPrintEvaluator = () => {
-  const body = document.querySelector("body");
-  body.classList.value = "";
-  body.innerHTML = document.querySelector("#printable").innerHTML;
-};
-
 module.exports = async (browser, ship, client) => {
   try {
     client.logger.info("getInvoices.start");
