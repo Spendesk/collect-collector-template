c7b96e5f7194ac6a0b56220c7415d1b6262c84bf
Auree Aubert <contact@auree.me>
2019-03-26 00:06:12 +0100

    Upgrade to spendesk-collect@0.0.11

--- template/app/actions/collect.js
+++ template/app/actions/collect.js
@@ -3,37 +3,13 @@ const signinUtil = require("../utils/signin");
 const getInvoicesUtil = require("../utils/get-invoices");
 const sendInvoicesUtil = require("../utils/send-invoices");
 
-const signIn = async (browser, ship, client) => {
-  try {
-    await signinUtil(browser, ship, client);
-
-    client.status("ok");
-
-    return true;
-  } catch (e) {
-    client.status("error", e.message);
-
-    return false;
-  }
-};
-
 module.exports = async (ship, client, captchaSolver) => {
   const browser = await browserUtil();
-  const isSignin = await signIn(browser, ship, client);
 
-  if (isSignin) {
-    try {
-      const invoices = await getInvoicesUtil(browser, ship, client);
+  await signinUtil(browser, ship, client);
 
-      await sendInvoicesUtil(invoices, client);
-
-      client.done();
-    } catch (e) {
-      client.done(true);
-    }
-  } else {
-    client.done(true);
-  }
+  const invoices = await getInvoicesUtil(browser, ship, client);
+  await sendInvoicesUtil(invoices, client);
 
   await browser.close();
 };
--- template/app/utils/signin.js
+++ template/app/utils/signin.js
@@ -1,23 +1,12 @@
-const _ = require("lodash");
-
-class SigninError extends Error {}
+const AuthError = require("spendesk-collect/errors/auth");
 
 module.exports = async (browser, ship, client) => {
-  try {
-    client.logger.info("signin.start");
+  client.logger.info("signin.start");
 
-    /*
-     * 1. Throw signin error when required settings are empty
-     * 2. Try to signin to the supplier
-     */
+  /*
+    * 1. Throw signin error when required settings are empty
+    * 2. Try to signin to the supplier
+    */
 
-
-    client.logger.info("signin.success");
-  } catch (e) {
-    client.logger.error("signin.error", { error: e.message });
-
-    throw new Error(
-      e.constructor.name === "SigninError" ? e.message : "Login failed"
-    );
-  }
+  client.logger.info("signin.success");
 };
